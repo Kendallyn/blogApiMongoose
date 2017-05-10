548c593a31e7f13778cedaac1fa675e3be4db60a
Kendallyn <Kendallynlyon@hotmail.com>
2017-05-10 15:49:59 -0700

    correct syntax

--- server.js
+++ server.js
@@ -29,109 +29,114 @@ app.get('/posts', (req, res) => {
     });
 });
 
+
 app.get('/posts/:id', (req, res) => {
-            BlogPost.findById(req.params.id).exec().then(post => res.json(post.apiRepr()))
-                .catch(err => {
-                    console.error(err);
-                    res.status(500).json({
-                        error: 'Internal server error'
-                    });
+    BlogPost.findById(req.params.id).exec().then(post => res.json(post.apiRepr()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                error: 'Internal server error'
+            });
+        });
+});
+
+
+app.post('/posts', (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length, i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    BlogPost.create({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author
+        })
+        .then(blogPost => res.status(201).json(blogPost.apiRepr()))
+        .catch(err => {
+            res.status(500).json({
+                error: 'Internal server error'
+            });
+        });
+});
+app.put('/post/:id', (req, res) => {
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        res.status(400).json({
+            error: 'Request path id and request body id values must match'
+        });
+    }
+    const updated = {};
+    const updateableFields = ['title', 'content', 'author'];
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            updated[field] = req.body[field];
+        }
+    });
+
+    BlogPost.findByIdAndUpdate(req.params.id {
+            $set: updated
+        }, {
+            new: true
+        }).exec()
+        .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
+        .catch(err => res.status(500).json({
+            message: 'Internal server error'
+        }));
+});
+app.delete('/posts/', (req, res) {
+    BlogPost.findByIdAndRemove(req.params.id).exec().then(console.log(`Deleted blog post with id \`${req.params.ID}\``); res.status(204).end();
+    });
+});
+app.use('*', function (req, res) {
+    res.status(404).json({
+        message: 'Not Found'
+    });
+});
+
+let server;
+
+
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                    console.log(`Your app is listening on port ${port}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
                 });
+        });
+    });
+}
 
-            app.post('/posts', (req, res) => {
-                        const requiredFields = ['title', 'content', 'author'];
-                        for (let i = 0; i < requiredFields.length, i++) {
-                            const field = requiredFields[i];
-                            if (!(field in req.body)) {
-                                const message = `Missing \`${field}\` in request body`
-                                console.error(message);
-                                return res.status(400).send(message);
-                            }
-                        }
-                        BlogPost.create({
-                                title: req.body.title,
-                                content: req.body.content,
-                                author: req.body.author
-                            })
-                            .then(blogPost => res.status(201).json(blogPost.apiRepr()))
-                            .catch(err => {
-                                res.status(500).json({
-                                    error: 'Internal server error'
-                                });
-                            });
-                        app.put('/post/:id', (req, res) => {
-                            if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-                                res.status(400).json({
-                                    error: 'Request path id and request body id values must match'
-                                });
-                            }
-                            const updated = {};
-                            const updateableFields = ['title', 'content', 'author'];
-                            updateableFields.forEach(field => {
-                                if (field in req.body) {
-                                    updated[field] = req.body[field];
-                                }
-                            });
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
 
-                            BlogPost.findByIdAndUpdate(req.params.id {
-                                    $set: updated
-                                }, {
-                                    new: true
-                                }).exec()
-                                .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
-                                .catch(err => res.status(500).json({
-                                    message: 'Internal server error'
-                                }));
-                        });
-                        app.delete('/posts/', (req, res) {
-                                BlogPost.findByIdAndRemove(req.params.id).exec().then(console.log(`Deleted blog post with id \`${req.params.ID}\``); res.status(204).end();
-                                });
-                        }); app.use('*', function (req, res) {
-                        res.status(404).json({
-                            message: 'Not Found'
-                        });
-                    });
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
 
-                    let server;
-
-
-                    function runServer(databaseUrl = DATABASE_URL, port = PORT) {
-
-                        return new Promise((resolve, reject) => {
-                            mongoose.connect(databaseUrl, err => {
-                                if (err) {
-                                    return reject(err);
-                                }
-                                server = app.listen(port, () => {
-                                        console.log(`Your app is listening on port ${port}`);
-                                        resolve();
-                                    })
-                                    .on('error', err => {
-                                        mongoose.disconnect();
-                                        reject(err);
-                                    });
-                            });
-                        });
-                    }
-
-                    function closeServer() {
-                        return mongoose.disconnect().then(() => {
-                            return new Promise((resolve, reject) => {
-                                console.log('Closing server');
-                                server.close(err => {
-                                    if (err) {
-                                        return reject(err);
-                                    }
-                                    resolve();
-                                });
-                            });
-                        });
-                    }
-
-                    if (require.main === module) {
-                        runServer().catch(err => console.error(err));
-                    };
-
-                    module.exports = {
-                        app, runServer, closeServer
-                    };
+module.exports = {
+    app, runServer, closeServer
+};
